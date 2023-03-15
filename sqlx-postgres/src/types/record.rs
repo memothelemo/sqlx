e5a831212ad5b47c00ebeae1c9f9b67a1f2a25db
memothelemo <memothelemo@protonmail.com>
2023-03-15 22:26:57 +0800

    Change its own type lifetime to same row lifetime in PgRecordDecorder::try_decode

--- sqlx-postgres/src/types/record.rs
+++ sqlx-postgres/src/types/record.rs
@@ -94,7 +94,8 @@ impl<'r> PgRecordDecoder<'r> {
     #[doc(hidden)]
     pub fn try_decode<T>(&mut self) -> Result<T, BoxDynError>
     where
-        T: for<'a> Decode<'a, Postgres> + Type<Postgres>,
+        T: Decode<'r, Postgres> + Type<Postgres>,
+//         T: for<'a> Decode<'a, Postgres> + Type<Postgres>,
     {
         if self.buf.is_empty() {
             return Err(format!("no field `{0}` found on record", self.ind).into());
